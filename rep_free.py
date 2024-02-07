388a6c6e84d61ee142a2f07e1967f95f9d70ace5
Aditya Ahirrao <adityaahirrao4503@gmail.com>
2024-02-07 21:48:12 +0530

    Add files via upload

---
+++ rep_free.py
@@ -0,0 +1,11 @@
+def repfree(s):
+    repeatation = True
+    for i in range(0,len(s)):
+        for j in range(0,len(s)):
+            if i!=j and s[i]==s[j]:
+                repeatation = False
+                break
+    return repeatation
+
+repfree("alpha_maverick")
+            
